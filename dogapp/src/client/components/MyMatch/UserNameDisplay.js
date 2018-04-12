6e3eb0ffdb269b69e766786041f1aeb75abd5db4
SaraRiach <saracar2@gmail.com>
2018-04-12 17:46:41 +0200

    Userdisplay finished

--- dogapp/src/client/components/MyMatch/UserNameDisplay.js
+++ dogapp/src/client/components/MyMatch/UserNameDisplay.js
@@ -5,14 +5,15 @@ import {User} from '../../../DummyData';
 
 
 export class UserNameDisplay extends PureComponent {
-  static propTypes = {
-    username: PropTypes.string.isRequired
-  }
+
   render() {
-    const username = this.props.user;
-  return (
-    <div>{username}</div>
-  )
+    console.log('name')
+return(
+    <div>
+      <h1>{User[0].username}
+      </h1>
+      </div>
+    )
   }
 }
 // const mapStateToProps = function (state, props) {
