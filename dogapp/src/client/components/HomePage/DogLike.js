339ab2ead0e8e99e2e1c5bd98956b82a777df94b
ArthurSlothouber <mailforarthur@yahoo.com>
2018-04-12 19:44:42 +0200

    unimportant changes

--- dogapp/src/client/components/HomePage/DogLike.js
+++ dogapp/src/client/components/HomePage/DogLike.js
@@ -1,13 +1,16 @@
 import React, { PureComponent } from 'react'
 
+
+
 class DogLike extends PureComponent {
   render() {
     return(
       <button className="homepagelikebutton">
-        Like 
+        Like
       </button>
     )
   }
 }
+
 
 export default DogLike
