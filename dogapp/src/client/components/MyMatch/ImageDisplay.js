dd865a67c5cf7f88abbaf721f0454684e334e1cb
ArthurSlothouber <mailforarthur@yahoo.com>
2018-04-12 11:47:07 +0200

    Sara components

---
+++ dogapp/src/client/components/MyMatch/ImageDisplay.js
@@ -0,0 +1,22 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+export class ImageDisplay extends PureComponent {
+static propTypes ={
+  imagedisplay: PropTypes.func.isRequired
+}
+  render() {
+return (
+  <div>
+
+  <img src="https://static.boredpanda.com/blog/wp-content/uploads/2017/09/dog-tinder-phil-lifewithmalamutes-fb__700-png.jpg" alt=""/>
+</div>
+  )
+  }
+}
+// const mapStateToProps = function (state, props) {
+//   return {
+//     userimage: state.userimage
+//   }
+// }
+export default ImageDisplay
