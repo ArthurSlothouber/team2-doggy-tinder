76e483fef80f44e26c1f5c6c05bb13e90ff6d019
neenu <neenukurian92@gmail.com>
2018-04-12 09:40:33 +0200

    components added

---
+++ dogapp/src/client/components/TopThree/BackButtonTopThree.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class BackButtonTopThree extends PureComponent {
+  render() {
+    return(
+      <button className="backtopthreebutton">
+        Back
+      </button>
+    )
+  }
+}
+
+export default BackButtonTopThree
